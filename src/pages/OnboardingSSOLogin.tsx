2c4a754f4996afe78bbabc9787da22358d6df7cd
waldls <p353700@gmail.com>
2025-07-03 18:41:26 +0900

    #1 [DEL] 불필요한 텍스트 속성 제거

--- src/pages/OnboardingSSOLogin.tsx
+++ src/pages/OnboardingSSOLogin.tsx
@@ -7,17 +7,13 @@ const OnboardingSSOLogin = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-[#F9FAFB]">
       {/* 백호 텍스트 */}
-      <h2 className="font-title-sb text-primary-blue w-[6.1rem] h-[3.6rem] leading-[3.6rem] tracking-[-0.048rem]">
-        Veco
-      </h2>
+      <h2 className="font-title-sb text-primary-blue">Veco</h2>
 
       {/* 백호 로고 */}
       <img src={vecologo} alt="Veco" className="w-[12.2rem] h-[11.1726rem] shrink-0 mt-[1.6rem]" />
 
       {/* "나의 작업실을 생성해봐요" 문구 */}
-      <h1 className=" font-body-r text-black w-[16.7rem] h-[2.2rem] leading-[2.24rem] tracking-[-0.032rem] mt-[3.13rem]">
-        나의 작업실을 생성해봐요
-      </h1>
+      <h1 className=" font-body-r text-black mt-[3.13rem]">나의 작업실을 생성해봐요</h1>
 
       {/* 소셜 로그인 버튼 영역 */}
       <div className="flex flex-col gap-[1.6rem] mt-[3.2rem]">
@@ -54,7 +50,7 @@ const OnboardingSSOLogin = () => {
 
       {/* 하단 이용약관 + 밑줄 */}
       <div className="flex flex-col gap-[0.1rem] mt-[3.2rem]">
-        <p className="font-body-r text-gray-400 w-[18.6rem] h-[2.1rem] leading-[2.24rem] tracking-[-0.032rem] text-center">
+        <p className="font-body-r text-gray-400 w-[18.6rem] h-[2.1rem] text-center">
           이용약관 & 데이터 처리 동의
         </p>
         <hr className=" bg-gray-400 w-[18.6rem] h-[0.1rem] border-none shrink-0" />
