619d49449b0e5df9a0a7a33f552e57a7d9df1d05
waldls <p353700@gmail.com>
2025-07-03 16:15:19 +0900

    #1 [UI] OnboardingSSOLogin 페이지 UI 작업

---
+++ src/pages/OnboardingSSOLogin.tsx
@@ -0,0 +1,66 @@
+import vecologo from '../assets/vecologo.svg';
+import googlelogo from '../assets/googlelogo.svg';
+import kakaologo from '../assets/kakaologo.svg';
+import '../index.css';
+
+const OnboardingSSOLogin = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-[#F9FAFB]">
+      {/* 백호 텍스트 */}
+      <h2 className="font-title-sb text-primary-blue w-[6.1rem] h-[3.6rem] leading-[3.6rem] tracking-[-0.048rem]">
+        Veco
+      </h2>
+
+      {/* 백호 로고 */}
+      <img src={vecologo} alt="Veco" className="w-[12.2rem] h-[11.1726rem] shrink-0 mt-[1.6rem]" />
+
+      {/* "나의 작업실을 생성해봐요" 문구 */}
+      <h1 className=" font-body-r text-black w-[16.7rem] h-[2.2rem] leading-[2.24rem] tracking-[-0.032rem] mt-[3.13rem]">
+        나의 작업실을 생성해봐요
+      </h1>
+
+      {/* 소셜 로그인 버튼 영역 */}
+      <div className="flex flex-col gap-[1.6rem] mt-[3.2rem]">
+        {/* 구글 로그인 버튼 */}
+        <button
+          disabled
+          className="relative w-[40rem] h-[6.4rem] rounded-[0.5rem] bg-gray-200 shrink-0"
+        >
+          <img
+            src={googlelogo}
+            alt="Google"
+            className="absolute left-[2.8rem] top-1/2 -translate-y-1/2 w-[2.4rem] h-[2.4rem]"
+          />
+          <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-body-r text-gray-400 w-[10.6rem] h-[2.2rem] leading-[2.24rem] tracking-[-0.032rem] text-center block">
+            구글로 계속하기
+          </span>
+        </button>
+
+        {/* 카카오 로그인 버튼 */}
+        <button
+          disabled
+          className="relative w-[40rem] h-[6.4rem] rounded-[0.5rem] bg-gray-200 shrink-0"
+        >
+          <img
+            src={kakaologo}
+            alt="Kakao"
+            className="absolute left-[2rem] top-1/2 -translate-y-1/2 w-[4.2rem] h-[4.2rem]"
+          />
+          <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-body-r text-gray-400 w-[14.9rem] h-[2.2rem] leading-[2.24rem] tracking-[-0.032rem] text-center block">
+            카카오톡으로 계속하기
+          </span>
+        </button>
+      </div>
+
+      {/* 하단 이용약관 + 밑줄 */}
+      <div className="flex flex-col gap-[0.1rem] mt-[3.2rem]">
+        <p className="font-body-r text-gray-400 w-[18.6rem] h-[2.1rem] leading-[2.24rem] tracking-[-0.032rem] text-center">
+          이용약관 & 데이터 처리 동의
+        </p>
+        <hr className=" bg-gray-400 w-[18.6rem] h-[0.1rem] border-none shrink-0" />
+      </div>
+    </div>
+  );
+};
+
+export default OnboardingSSOLogin;
