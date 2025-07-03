a237858f8a96ac0665abfdf71624e610a3f2f927
waldls <p353700@gmail.com>
2025-07-03 19:11:07 +0900

    #1 #1 [DEL] 불필요한 텍스트 속성 제거

--- src/pages/OnboardingSSOLogin.tsx
+++ src/pages/OnboardingSSOLogin.tsx
@@ -27,7 +27,7 @@ const OnboardingSSOLogin = () => {
             alt="Google"
             className="absolute left-[2.8rem] top-1/2 -translate-y-1/2 w-[2.4rem] h-[2.4rem]"
           />
-          <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-body-r text-gray-400 w-[10.6rem] h-[2.2rem] leading-[2.24rem] tracking-[-0.032rem] text-center block">
+          <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-body-r text-gray-400 text-center block">
             구글로 계속하기
           </span>
         </button>
@@ -42,7 +42,7 @@ const OnboardingSSOLogin = () => {
             alt="Kakao"
             className="absolute left-[2rem] top-1/2 -translate-y-1/2 w-[4.2rem] h-[4.2rem]"
           />
-          <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-body-r text-gray-400 w-[14.9rem] h-[2.2rem] leading-[2.24rem] tracking-[-0.032rem] text-center block">
+          <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-body-r text-gray-400 text-center block">
             카카오톡으로 계속하기
           </span>
         </button>
@@ -50,9 +50,7 @@ const OnboardingSSOLogin = () => {
 
       {/* 하단 이용약관 + 밑줄 */}
       <div className="flex flex-col gap-[0.1rem] mt-[3.2rem]">
-        <p className="font-body-r text-gray-400 w-[18.6rem] h-[2.1rem] text-center">
-          이용약관 & 데이터 처리 동의
-        </p>
+        <p className="font-body-r text-gray-400 text-center">이용약관 & 데이터 처리 동의</p>
         <hr className=" bg-gray-400 w-[18.6rem] h-[0.1rem] border-none shrink-0" />
       </div>
     </div>
