5cb264c5c50f0dd6355367bf840e51b32c4e92d5
jinj00oo <icems0428@naver.com>
2025-07-07 01:21:41 +0900

    #5 [STYLE] 버튼 스타일 수정
    -  SidebarItem 컴포넌트 전체 div에 hover 적용하는 것으로 묶음
    - + 버튼 hover 시 bg 더 진해지도록 구현

--- src/components/Sidebar/SidebarItem.tsx
+++ src/components/Sidebar/SidebarItem.tsx
@@ -9,18 +9,22 @@ interface SidebarItemProps {
 
 const SidebarItem = ({ icon, label, onClick, onAddClick }: SidebarItemProps) => {
   return (
-    <div className="flex w-full items-center self-stretch">
+    <div className="flex w-full items-center self-stretch hover:bg-gray-200 rounded-xl">
       <button
         onClick={onClick}
         type="button"
-        className="flex grow items-center gap-[0.8rem] hover:bg-gray-200 rounded-xl cursor-pointer"
+        className="flex grow items-center gap-[0.8rem] cursor-pointer"
       >
         <span className="w-[2.4rem] h-[2.4rem] shrink-0 aspect-square">{icon}</span>
         <span className="font-small-r text-gray-600 letter-spacing-[0.028rem]">{label}</span>
       </button>
       {onAddClick && (
-        <button onClick={onAddClick} type="button" className="cursor-pointer">
-          <img src={plusIcon} alt="Add" className="w-[2.4rem] h-[2.4rem] aspect-square hover:bg-gray-200 rounded-full" />
+        <button
+          onClick={onAddClick}
+          type="button"
+          className="hover:bg-gray-300 rounded-xl cursor-pointer"
+        >
+          <img src={plusIcon} alt="Add" className="w-[2.4rem] h-[2.4rem] aspect-square" />
         </button>
       )}
     </div>
