738c24a532ea57068bec0b0fd241750eecb0cfbc
Lee Gaeul <91470334+gaeulzzang@users.noreply.github.com>
2025-07-07 22:41:40 +0900

    #26 [FEAT] 모달 상태 zustand로 관리

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
-import Modal from './components/Modal/Modal.tsx';
 
 function App() {
-  return (
-    // <RouterProvider router={router} />
-    <Modal subtitle={'최대 20글자까지 작성할 수 있습니다'} />
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
