e0082924ae9a44a3db294b9f0697dec398f021b8
jinj00oo <icems0428@naver.com>
2025-07-08 00:20:51 +0900

    #25 [FEAT] 라우트에 Outlet 추가 및 사이드바 스크롤 처리
    - 자식 요소가 보일 수 있도록 element에 Outlet 추가
    - 사이드바 스크롤 가능하도록 처리

--- src/layouts/ProtectedLayout.tsx
+++ src/layouts/ProtectedLayout.tsx
@@ -3,8 +3,10 @@ import Sidebar from '../components/Sidebar/Sidebar';
 
 const ProtectedLayout = () => {
   return (
-    <div className="flex">
-      <Sidebar />
+    <div className="flex h-screen">
+      <aside className="overflow-auto">
+        <Sidebar />
+      </aside>
       <main className="flex-1 min-w-0 p-[3.2rem] overflow-auto">
         <Outlet />
       </main>
