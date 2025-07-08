18f3b1dac65eb4b08deaa1771348008a8497f182
Lee Gaeul <91470334+gaeulzzang@users.noreply.github.com>
2025-07-08 18:09:30 +0900

    #29 [FEAT] 드롭다운 상태 zustand로 관리

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,33 @@ import { RouterProvider } from 'react-router-dom';
 import router from './routes';
 import Dropdown from './components/Dropdown/Dropdown.tsx';
 import { useState } from 'react';
+import { useDropdownActions, useDropdownInfo } from './hooks/useDropdown.ts';
 
 function App() {
   // return <RouterProvider router={router} />;
   const [value, setValue] = useState('우선순위');
-  const [isOpen, setIsOpen] = useState(false);
+  const { isOpen, content } = useDropdownInfo();
+  const { openDropdown, closeDropdown } = useDropdownActions();
 
   return (
     <div className={`flex justify-center items-center h-screen`}>
       <div className={`relative`}>
-        <span className={`font-title-sub-r`} onClick={() => setIsOpen(true)}>
+        <span
+          className={`font-title-sub-r`}
+          onClick={() => {
+            openDropdown({ name: '우선순위' });
+          }}
+        >
           {value}
         </span>
-        {isOpen && (
+        {isOpen && content && (
           <Dropdown
             defaultValue={'우선순위'}
             options={['없음', '해야할 일', '진행 중', '완료', '검토']}
             onSelect={(option) => {
               setValue(option);
-              setIsOpen(false);
             }}
+            onClose={closeDropdown}
           />
         )}
       </div>
--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,19 +1,42 @@
 import IcCheck from '../../assets/icons/check.svg';
 import IcDownArrow from '../../assets/icons/down-arrow.svg';
+import { useEffect, useRef } from 'react';
 
 interface DropdownProps {
   defaultValue: string;
   options: string[];
   onSelect: (value: string) => void;
+  onClose: () => void;
 }
 
-const Dropdown = ({ defaultValue, options, onSelect }: DropdownProps) => {
+const Dropdown = ({ defaultValue, options, onSelect, onClose }: DropdownProps) => {
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        onClose();
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [onClose]);
+
   return (
     <div
+      ref={dropdownRef}
       className={`absolute top-0 left-0 flex flex-col w-[11.6rem] 
       border border-gray-400 bg-white rounded-[0.4rem] shadow-lg`}
     >
-      <div className={`flex justify-between items-center py-[0.4rem] ps-[1.2rem] pe-[0.8rem]`}>
+      <div
+        className={`flex justify-between items-center py-[0.4rem] ps-[1.2rem] pe-[0.8rem]`}
+        onClick={() => {
+          onSelect(defaultValue);
+          onClose();
+        }}
+      >
         <span className={`font-xsmall-r text-gray-600 me-[0.4rem]`}>{defaultValue}</span>
         <img src={IcDownArrow} alt={defaultValue} />
       </div>
@@ -21,7 +44,10 @@ const Dropdown = ({ defaultValue, options, onSelect }: DropdownProps) => {
         <div
           key={option}
           className={`flex group hover:bg-gray-200 py-[0.75rem] px-[1.2rem]`}
-          onClick={() => onSelect(option)}
+          onClick={() => {
+            onSelect(option);
+            onClose();
+          }}
         >
           <span className={`font-xsmall-r text-gray-600 me-[0.4rem]`}>{option}</span>
           <img className={`opacity-0 group-hover:opacity-100`} src={IcCheck} alt={option} />
