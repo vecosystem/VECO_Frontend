7220380e0290b88995a515b46e54610ef9378072
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-07-08 21:32:27 +0900

    #23 [DEL] 드롭다운 파일 제거
    다른 공용 컴포넌트 사용 예정

--- src/pages/goal/GoalHome.tsx
+++ src/pages/goal/GoalHome.tsx
@@ -10,7 +10,6 @@ import {
   type GoalItemProps,
   type ItemFilter,
 } from '../../types/listItem';
-import { GoalFilterDropdown } from '../../components/ListView/GoalFilterDropdown';
 
 /*
   추후 더미데이터 대신 실제 api 명세서 참고하여 수정 예정
@@ -75,10 +74,6 @@ const GoalHome = () => {
             {/* 필터영역 */}
             <div className="flex gap-[0.8rem] items-center cursor-pointer relative">
               {/* 드롭다운 */}
-              <GoalFilterDropdown
-                value={filter}
-                onChange={(type) => setFilter(type as ItemFilter)}
-              />
             </div>
             <div className="flex gap-[0.4rem] items-center">
               <img src={TrashIcon} className="inline-block w-[2.4rem] h-[2.4rem]" alt="" />
@@ -90,15 +85,18 @@ const GoalHome = () => {
         {/* 리스트 뷰 */}
         <div className="flex flex-col gap-[6.4rem]">
           {grouped.map(({ key, items }) =>
+            /* 해당 요소 존재할 때만 생성 */
             items.length > 0 ? (
               <div key={key}>
                 <div className="flex justify-between pb-[3.2rem]">
                   <div className="flex font-title-sub-b gap-[0.8rem]">
+                    {/* 유형명 */}
                     <div>{key}</div>
                     <div className="text-gray-500">{items.length}</div>
                   </div>
                   <img src={PlusIcon} className="inline-block w-[2.4rem] h-[2.4rem]" alt="" />
                 </div>
+                {/* 각 유형 별 요소 */}
                 {items.map((goal) => (
                   <GoalItem key={goal.goalId} {...goal} filter={filter} />
                 ))}
