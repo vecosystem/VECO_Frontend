f60294410959f70decd2b2cc9a01b9a6b3b71f22
Lee Gaeul <91470334+gaeulzzang@users.noreply.github.com>
2025-07-10 18:04:46 +0900

    #32 [REFACTOR] 드롭다운 기능 리팩토링

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -3,28 +3,40 @@ import IcDownArrow from '../../assets/icons/down-arrow.svg';
 import useDropdownRef from '../../hooks/useDropdownRef.ts';
 
 interface DropdownProps {
+  value?: string;
   defaultValue?: string;
   options: string[];
   onSelect: (value: string) => void;
   onClose: () => void;
+  className?: string;
 }
 
-const Dropdown = ({ defaultValue, options, onSelect, onClose }: DropdownProps) => {
+const Dropdown = ({
+  value,
+  defaultValue,
+  options,
+  onSelect,
+  onClose,
+  className,
+}: DropdownProps) => {
   const dropdownRef = useDropdownRef(onClose);
+
+  const handleSelect = (option: string) => {
+    defaultValue && onSelect(option);
+    onClose();
+  };
+
   return (
     <div
       ref={dropdownRef}
       style={{ boxShadow: '0px 4px 12px 0px rgba(0, 0, 0, 0.15)' }}
       className={`absolute z-30 top-0 left-0 flex flex-col w-auto min-w-[11.6rem] max-w-[27.4rem]
-      border border-gray-400 bg-white rounded-[0.4rem]`}
+      border border-gray-400 bg-white rounded-[0.4rem] ${className}`}
     >
       {defaultValue && (
         <div
           className={`flex border-b border-gray-200 justify-between items-center py-[0.4rem] ps-[1.2rem] pe-[0.8rem]`}
-          onClick={() => {
-            onSelect(defaultValue);
-            onClose();
-          }}
+          onClick={() => onClose()}
         >
           <span className={`font-xsmall-r text-gray-600 me-[0.4rem]`}>{defaultValue}</span>
           <img src={IcDownArrow} alt={defaultValue} />
@@ -33,15 +45,12 @@ const Dropdown = ({ defaultValue, options, onSelect, onClose }: DropdownProps) =
       {options.map((option) => (
         <div
           key={option}
-          className={`flex ${defaultValue && 'group hover:bg-gray-200'} py-[0.75rem] px-[1.2rem]`}
-          onClick={() => {
-            defaultValue && onSelect(option);
-            onClose();
-          }}
+          className={`flex py-[0.75rem] px-[1.2rem] ${value === option ? 'bg-gray-200' : ''}`}
+          onClick={() => handleSelect(option)}
         >
           <span className={`font-xsmall-r text-gray-600 me-[0.4rem] truncate`}>{option}</span>
           <img
-            className={`opacity-0 ${defaultValue && 'group-hover:opacity-100'}`}
+            className={`opacity-0 ${value === option ? 'opacity-100' : 'opacity-0'}`}
             src={IcCheck}
             alt={option}
           />
