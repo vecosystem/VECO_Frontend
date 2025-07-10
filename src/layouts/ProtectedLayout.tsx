80d65db34a75e1530d2fbe0d4772a2926f599b5f
jinj00oo <icems0428@naver.com>
2025-07-11 02:09:06 +0900

    #39 [FEAT] 스크롤바 커스텀

--- src/layouts/ProtectedLayout.tsx
+++ src/layouts/ProtectedLayout.tsx
@@ -4,10 +4,10 @@ import Sidebar from '../components/Sidebar/Sidebar';
 const ProtectedLayout = () => {
   return (
     <div className="flex h-screen">
-      <aside className="overflow-auto">
+      <aside className="overflow-auto scrollbar">
         <Sidebar />
       </aside>
-      <main className="flex-1 min-w-0 p-[3.2rem] overflow-auto">
+      <main className="flex-1 min-w-0 p-[3.2rem] overflow-auto scrollbar">
         <Outlet />
       </main>
     </div>
