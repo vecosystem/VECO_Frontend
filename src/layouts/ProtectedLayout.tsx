ff21691d215ddd66246252421a06d58564c1b777
jinj00oo <icems0428@naver.com>
2025-07-11 02:32:34 +0900

    #39 [DEL] scrollbar 속성 삭제
    - 삭제해도 적용됨

--- src/layouts/ProtectedLayout.tsx
+++ src/layouts/ProtectedLayout.tsx
@@ -4,10 +4,10 @@ import Sidebar from '../components/Sidebar/Sidebar';
 const ProtectedLayout = () => {
   return (
     <div className="flex h-screen">
-      <aside className="overflow-auto scrollbar">
+      <aside className="overflow-auto">
         <Sidebar />
       </aside>
-      <main className="flex-1 min-w-0 p-[3.2rem] overflow-auto scrollbar">
+      <main className="flex-1 min-w-0 p-[3.2rem] overflow-auto">
         <Outlet />
       </main>
     </div>
