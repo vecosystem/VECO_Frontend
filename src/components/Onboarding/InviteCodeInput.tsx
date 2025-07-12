cea152e172a25f9c4bdbffaa5882300e418dc559
waldls <p353700@gmail.com>
2025-07-13 02:13:57 +0900

    #45 [ADD] InviteCodeInput 컴포넌트 코드 주석 추가

--- src/components/Onboarding/InviteCodeInput.tsx
+++ src/components/Onboarding/InviteCodeInput.tsx
@@ -5,13 +5,14 @@ interface InviteCodeInputProps {
 }
 
 const InviteCodeInput = ({ correctCode }: InviteCodeInputProps) => {
-  const [inputCode, setInputCode] = useState('');
-  const [hasError, setHasError] = useState(false);
+  const [inputCode, setInputCode] = useState(''); // 사용자가 입력한 암호를 저장하는 상태 변수
+  const [hasError, setHasError] = useState(false); // 에러 상태를 저장하는 변수
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const typedcode = e.target.value;
-    setInputCode(typedcode);
+    const typedcode = e.target.value; // 사용자가 입력한 암호
+    setInputCode(typedcode); // 상태 업데이트
 
+    // 입력한 암호가 있고, 초대한 사람이 준 암호와 다르면 에러
     if (typedcode && typedcode !== correctCode) {
       setHasError(true);
     } else {
@@ -21,6 +22,7 @@ const InviteCodeInput = ({ correctCode }: InviteCodeInputProps) => {
 
   return (
     <div className="flex flex-col gap-[1rem]">
+      {/* 텍스트 입력창 */}
       <input
         type="text"
         value={inputCode}
@@ -29,9 +31,9 @@ const InviteCodeInput = ({ correctCode }: InviteCodeInputProps) => {
         className={`
       w-[40rem] h-[6.2rem] px-[2rem] py-[1.8rem] font-body-r rounded-[0.5rem]
       focus:outline-none text-gray-400 placeholder:text-gray-400
-      ${hasError ? 'border border-[#D44242] bg-[#F9FAFB]' : 'bg-gray-200 border border-transparent'}
-    `}
+      ${hasError ? 'border border-[#D44242] bg-[#F9FAFB]' : 'border bg-gray-200 border-transparent'}`}
       />
+      {/* 에러 메시지 */}
       <span className={`font-xsmall-r ${hasError ? 'text-[#D44242]' : 'text-transparent'}`}>
         입력 정보를 다시 확인하세요
       </span>
