9a82978453a013e0e8a0481e8fc6d230e3fedbf1
Lee Gaeul <91470334+gaeulzzang@users.noreply.github.com>
2025-07-13 23:16:46 +0900

    #49 [ADD] 유저 검색 아이콘 임포트

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
+import SettingTeam from './pages/setting/SettingTeam.tsx';
 
 function App() {
-  return <RouterProvider router={router} />;
+  // return <RouterProvider router={router} />;
+  return <SettingTeam />;
 }
 
 export default App;
