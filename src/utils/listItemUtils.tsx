c953e5f30db2ff211dd6ba782ea3c0cf93a62ba6
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-07-14 17:57:22 +0900

    #51 [FEAT] 타입 enum 적용

--- src/utils/listItemUtils.tsx
+++ src/utils/listItemUtils.tsx
@@ -4,7 +4,7 @@ import pr2 from '../assets/icons/pr-2.svg';
 import pr3 from '../assets/icons/pr-3.svg';
 import pr4 from '../assets/icons/pr-4.svg';
 
-import type { ItemFilter, DisplayField, PriorityLevel, Status } from '../types/listItem';
+import type { ItemFilter, DisplayField, StatusCode, PriorityCode } from '../types/listItem';
 
 /* 필터 적용 */
 export const getFilter = (filter: ItemFilter = '상태'): DisplayField[] => {
@@ -25,25 +25,26 @@ export const getFilter = (filter: ItemFilter = '상태'): DisplayField[] => {
 // TODO : 각 아이콘 사이즈 변경 및 색상 변경 확인
 
 /* 우선순위 아이콘 */
-export const getPriorityIcon = (priority: PriorityLevel): string => {
-  const iconMap: Record<PriorityLevel, string> = {
-    없음: pr0,
-    낮음: pr1,
-    보통: pr2,
-    높음: pr3,
-    긴급: pr4,
+export const getPriorityIcon = (priority: PriorityCode): string => {
+  const iconMap: Record<PriorityCode, string> = {
+    NONE: pr0,
+    LOW: pr1,
+    NORMAL: pr2,
+    HIGH: pr3,
+    URGENT: pr4,
   };
   return iconMap[priority];
 };
 
 /* 상태 아이콘 및 색상 */
-export const getStatusIcon = (status: Status) => {
-  const colorMap: Record<Status, string> = {
-    없음: '#DCDCDC',
-    진행중: '#D4B042',
-    '해야할 일': '#D44242',
-    완료: '#75D564',
-    검토: '#2e4475',
+export const getStatusIcon = (status: StatusCode) => {
+  const colorMap: Record<StatusCode, string> = {
+    NONE: '#DCDCDC',
+    DOING: '#D4B042',
+    TODO: '#D44242',
+    FINISH: '#75D564',
+    REVIEW: '#2e4475',
+    DELETE: '#D44242',
   };
   return (
     <span
@@ -54,13 +55,14 @@ export const getStatusIcon = (status: Status) => {
 };
 
 /* 상태 아이콘 색상만 반환 */
-export const getStatusColor = (status: Status): string => {
-  const colorMap: Record<Status, string> = {
-    없음: '#DCDCDC',
-    진행중: '#D4B042',
-    '해야할 일': '#D44242',
-    완료: '#75D564',
-    검토: '#2e4475',
+export const getStatusColor = (status: StatusCode): string => {
+  const colorMap: Record<StatusCode, string> = {
+    NONE: '#DCDCDC',
+    DOING: '#D4B042',
+    TODO: '#D44242',
+    FINISH: '#75D564',
+    REVIEW: '#2e4475',
+    DELETE: '#D44242',
   };
   return colorMap[status];
 };
