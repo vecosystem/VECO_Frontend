d801625aebc99bd5d832c6d9e5126c48a012e906
waldls <p353700@gmail.com>
2025-07-14 21:13:15 +0900

    #52 [FEAT] InviteCodeInput.tsx 컴포넌트에서 버튼 클릭 시 유효성 검사 로직 추가

--- src/pages/onboarding/ParticipateWorkspaceInputPw.tsx
+++ src/pages/onboarding/ParticipateWorkspaceInputPw.tsx
@@ -1,10 +1,29 @@
 // src/pages/onboarding/ParticipateWorkspaceInputPw.tsx
 
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import vecocirclenavy from '../../assets/logos/veco-circle-logo-bg-navy.svg';
 import InviteCodeInput from '../../components/Onboarding/InviteCodeInput';
 import PrimaryButton from '../../components/Onboarding/PrimaryButton';
 
 const ParticipateWorkspaceInputPw = () => {
+  const [inputCode, setInputCode] = useState(''); // 사용자 입력 코드 상태
+  const [hasError, setHasError] = useState(false); // 사용자 입력 코드 에러 상태
+  const navigate = useNavigate(); // 페이지 이동을 위한 훅
+  const correctCode = 'tempcode'; // 올바른 초대 코드 (백엔드 연동 전 임시 하드코딩)
+
+  // 입장하기 버튼 클릭 시 처리 로직
+  const handleClick = () => {
+    if (inputCode === correctCode) {
+      // 옳은 코드 - 에러 제거 및 워크스페이스 홈으로 이동
+      setHasError(false);
+      navigate('/workspace/home'); // 라우팅 확정 후 수정
+    } else {
+      // 잘못된 코드 - 에러 표시
+      setHasError(true);
+    }
+  };
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-[#F9FAFB]">
       <div className="flex flex-col gap-[6.9rem]">
@@ -14,10 +33,10 @@ const ParticipateWorkspaceInputPw = () => {
           {/* 초대 문구 - 백엔드 연동 후 수정 */}
           <h2 className="font-title-b text-gray-600">OOO 워크스페이스에 초대합니다.</h2>
           {/* 암호 입력란 - 백엔드 연동 후 수정 */}
-          <InviteCodeInput correctCode="tempcode" />
+          <InviteCodeInput inputCode={inputCode} onChange={setInputCode} hasError={hasError} />
         </div>
-        {/* 입장하기 버튼 - 추후 경로 연결 */}
-        <PrimaryButton text="입장하기" />
+        {/* 입장하기 버튼 */}
+        <PrimaryButton text="입장하기" onClick={handleClick} />
       </div>
     </div>
   );
