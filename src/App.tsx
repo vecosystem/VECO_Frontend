e9559cd44baa00e94ffd05ac6347b7c5c008df95
Lee Gaeul <91470334+gaeulzzang@users.noreply.github.com>
2025-07-14 17:10:30 +0900

    #49 [DEL] App.tsx 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
-import SettingTeam from './pages/setting/SettingTeam.tsx';
-import SettingMember from './pages/setting/SettingMember.tsx';
 
 function App() {
-  // return <RouterProvider router={router} />;
-  return <SettingTeam />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
