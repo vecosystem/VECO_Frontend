cda0dcde345c499e75add096d4591e252639edd5
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-07-17 01:01:36 +0900

    #51 [FEAT] 이슈 목록 조회 api 응답 구조

---
+++ src/types/issue.ts
@@ -0,0 +1,37 @@
+import type { CursorBasedResponse } from './common';
+
+/* 임시 구조, api 명세서 완성 시 변경 필요하면 수정 예정 */
+
+export type Deadline = {
+  start: string;
+  end: string;
+};
+
+export type ManagerInfo = {
+  profileUrl: string;
+  name: string;
+};
+
+export type Manager = {
+  cnt: number;
+  info: ManagerInfo[];
+};
+
+export type Issue = {
+  id: number;
+  name: string;
+  title: string;
+  status?: string;
+  priority?: string;
+  goaltitle?: string;
+  deadline: Deadline;
+  managers?: Manager;
+};
+
+export type IssueFilter = {
+  filterName: string;
+  dataCnt: number;
+  issues: Issue[];
+};
+
+export type ResponseIssueDto = CursorBasedResponse<IssueFilter[]>;
