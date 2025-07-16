985e1de694a7c6dcb9a402c3039a957546bcfe17
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-07-16 23:32:23 +0900

    #51 [FEAT] 공통 api 응답 구조

---
+++ src/types/goal.ts
@@ -0,0 +1,34 @@
+import type { CursorBasedResponse } from './common';
+
+export type Deadline = {
+  start: string;
+  end: string;
+};
+
+export type ManagerInfo = {
+  profileUrl: string;
+  name: string;
+};
+
+export type Manager = {
+  cnt: number;
+  info: ManagerInfo[];
+};
+
+export type Goal = {
+  id: number;
+  name: string;
+  title: string;
+  status?: string;
+  priority?: string;
+  deadline: Deadline;
+  managers?: Manager;
+};
+
+export type GoalFilter = {
+  filterName: string;
+  dataCnt: number;
+  goals: Goal[];
+};
+
+export type ResponseGoalDto = CursorBasedResponse<GoalFilter[]>;
