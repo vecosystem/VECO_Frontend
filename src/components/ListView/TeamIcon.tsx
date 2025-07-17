d2bfc88f2f220db09183ef6a4204c5cf9005ff67
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-07-18 01:42:28 +0900

    #61 [MOD] 타이틀 컴포넌트 이미지 수정

--- src/components/ListView/TeamIcon.tsx
+++ src/components/ListView/TeamIcon.tsx
@@ -1,3 +1,5 @@
+import logo from '../../assets/logos/veco-circle-logo-bg-white.svg';
+
 interface TeamIconProps {
   teamName?: string;
   teamImgUrl?: string;
@@ -8,13 +10,8 @@ const TeamIcon = ({ teamName, teamImgUrl }: TeamIconProps) => {
     <>
       <div className="flex gap-[1.6rem] items-center">
         <span
-          className="inline-block w-[3.2rem] h-[3.2rem] rounded-full border-[0.1rem] border-gray-200 bg-gray-100 bg-center bg-cover"
-          style={{
-            backgroundImage: `url(${teamImgUrl || '/vecosmall.svg'})`,
-            backgroundSize: '50%',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center',
-          }}
+          className="inline-block w-[3.2rem] h-[3.2rem] rounded-full bg-center bg-no-repeat bg-cover"
+          style={{ backgroundImage: `url("${teamImgUrl || logo}")` }}
         />
         <div className="flex font-title-b">{teamName || 'Team'}</div>
       </div>
