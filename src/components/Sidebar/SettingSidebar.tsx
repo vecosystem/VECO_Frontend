0c624dceb6c3f852f0a17595bd2dc9bddf037320
jinj00oo <icems0428@naver.com>
2025-07-18 17:35:44 +0900

    #63 [FEAT] 설정 사이드바 UI 및 버튼 클릭시 라우팅 구현
    - 사이드바 아이템 클릭시 알맞는 페이지로 라우팅

---
+++ src/components/Sidebar/SettingSidebar.tsx
@@ -0,0 +1,84 @@
+import SidebarItem from './SidebarItem';
+import DropdownMenu from './DropdownMenu';
+import { useNavigate } from 'react-router-dom';
+import leftArrowIcon from '../../assets/icons/left-arrow.svg';
+import vecocirclenavy from '../../assets/logos/veco-circle-logo-bg-navy.svg';
+import tableHoverIcon from '../../assets/icons/table-hover.svg';
+import usersIcon from '../../assets/icons/users.svg';
+import usersHoverIcon from '../../assets/icons/users-hover.svg';
+import userProfileIcon from '../../assets/icons/user-profile.svg';
+import userProfileHoverIcon from '../../assets/icons/user-profile-hover.svg';
+const SettingSidebar = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="w-[32rem] bg-gray-200 p-[3.2rem] shadow-lg min-h-screen">
+      <div className="flex flex-col items-start gap-[3.2rem] self-stretch">
+        <div className="flex justify-between items-start self-stretch">
+          <button
+            type="button"
+            className="flex items-center gap-[0.8rem] cursor-pointer"
+            onClick={() => {
+              navigate('/workspace');
+            }}
+          >
+            <img src={leftArrowIcon} className="w-[3.2rem] h-[3.2rem]" alt="Workspace" />
+            <span className="font-title-sub-b text-gray-600 letter-spacing-[-0.04rem]">홈</span>
+          </button>
+        </div>
+
+        <div className="flex flex-col items-start self-stretch">
+          <DropdownMenu headerTitle="워크스페이스" initialOpen={true}>
+            <div className="flex flex-col justify-center items-flex-start gap-[1.6rem] pb-[1.6rem]">
+              <SidebarItem
+                defaultIcon={<img src={vecocirclenavy} alt="Goal" />}
+                label="워크스페이스 프로필"
+                onClick={() => {
+                  navigate('/workspace/setting');
+                }}
+              />
+            </div>
+          </DropdownMenu>
+        </div>
+        <div className="flex flex-col items-start self-stretch">
+          <DropdownMenu headerTitle="팀" initialOpen={true}>
+            <div className="flex flex-col justify-center items-start gap-[1.6rem] pb-[1.6rem]">
+              <SidebarItem
+                defaultIcon={<img src={tableHoverIcon} alt="Goal" />}
+                hoverIcon={<img src={tableHoverIcon} alt="Goal" />}
+                label="팀 목록"
+                onClick={() => {
+                  navigate('/workspace/setting/team-list');
+                }}
+              />
+              <SidebarItem
+                defaultIcon={<img src={usersIcon} alt="Issue" />}
+                hoverIcon={<img src={usersHoverIcon} alt="Issue" />}
+                label="워크스페이스 멤버"
+                onClick={() => {
+                  navigate('/workspace/setting/team-members');
+                }}
+              />
+            </div>
+          </DropdownMenu>
+        </div>
+        <div className="flex flex-col items-start self-stretch">
+          <DropdownMenu headerTitle="계정" initialOpen={true}>
+            <div className="flex flex-col justify-center items-start gap-[1.6rem] pb-[1.6rem]">
+              <SidebarItem
+                defaultIcon={<img src={userProfileIcon} alt="Goal" />}
+                hoverIcon={<img src={userProfileHoverIcon} alt="Goal" />}
+                label="나의 프로필"
+                onClick={() => {
+                  navigate('/workspace/setting/my-profile');
+                }}
+              />
+            </div>
+          </DropdownMenu>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SettingSidebar;
