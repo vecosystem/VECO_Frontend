6af061186dc455ec01ccdbab95e1d1587848d030
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-07-21 17:54:34 +0900

    #69 [FEAT] 외부 필터 추가

--- src/types/listItem.ts
+++ src/types/listItem.ts
@@ -1,3 +1,4 @@
+import type { External } from './external';
 import type { Goal } from './goal';
 import type { Issue } from './issue';
 
@@ -24,9 +25,18 @@ export const PRIORITY_LABELS: Record<PriorityCode, string> = {
   LOW: '낮음',
 };
 
+// 외부 코드
+export const EXTERNAL_CODES = ['GITHUB', 'SLACK'] as const;
+export type ExternalCode = (typeof EXTERNAL_CODES)[number];
+export const EXTERNAL_LABELS: Record<ExternalCode, string> = {
+  GITHUB: 'GitHub',
+  SLACK: 'Slack',
+};
+
 // 리스트
 export const STATUS_LIST: readonly StatusCode[] = STATUS_CODES;
 export const PRIORITY_LIST: readonly PriorityCode[] = PRIORITY_CODES;
+export const EXTERNAL_LIST: readonly ExternalCode[] = EXTERNAL_CODES;
 
 export const FILTER_MAP = {
   상태: 'state',
@@ -39,7 +49,7 @@ export const FILTER_MAP = {
 export type ItemFilter = keyof typeof FILTER_MAP;
 
 // 공통 타입
-export type DisplayField = 'status' | 'priority' | 'deadline' | 'manage' | 'goal';
+export type DisplayField = 'status' | 'priority' | 'deadline' | 'manage' | 'goal' | 'external';
 
 // 공통 props
 export interface BaseItemProps {
@@ -55,5 +65,9 @@ export interface GoalItemProps extends Goal, BaseItemProps {
 }
 
 export interface IssueItemProps extends Issue, BaseItemProps {
+  variant?: 'default' | 'notification';
+}
+
+export interface ExternalItemProps extends External, BaseItemProps {
   variant?: 'default' | 'notification';
 }
