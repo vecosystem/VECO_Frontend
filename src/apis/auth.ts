ef3084b15119759f7ee0dedddaa8dec4ec92b06c
waldls <p353700@gmail.com>
2025-07-25 20:18:57 +0900

    #76 [FEAT] accessToken 재발급 요청 함수 구현

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,3 +1,25 @@
+import { axiosInstance } from './axios';
+import type { ReIssueTokenResponse } from '../types/auth';
+
+/**
+ * accessToken 재발급 요청 함수
+ */
+export const reissueAccessToken = async (): Promise<string> => {
+  const { data }: { data: ReIssueTokenResponse } = await axiosInstance.post(
+    '/api/token/reissue',
+    null,
+    {
+      withCredentials: true, // 쿠키 포함 (refreshToken 전송)
+    }
+  );
+
+  if (!data.result?.accessToken) {
+    throw new Error('accessToken 재발급 실패');
+  }
+
+  return data.result.accessToken;
+};
+
 /**
  * Google OAuth2 로그인 URL로 리다이렉트
  */
