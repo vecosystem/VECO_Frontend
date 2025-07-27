c0de0d2b4cf3d1e6ab4e3e9b3e0bc736239a47ad
jinj00oo <icems0428@naver.com>
2025-07-27 21:23:17 +0900

    #64 [UI] 로그아웃 모달 구현

--- src/pages/setting/SettingMyProfile.tsx
+++ src/pages/setting/SettingMyProfile.tsx
@@ -7,6 +7,7 @@ import CheckBoxIcon from '../../assets/icons/check-box-x.svg';
 import CheckBoxRedIcon from '../../assets/icons/check-box-o-red.svg';
 import { useState, useRef } from 'react';
 import { useModalActions, useModalInfo } from '../../hooks/useModal.ts';
+import Modal from '../../components/Modal/Modal.tsx';
 const SettingMyProfile = () => {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
   const { isOpen, content } = useModalInfo();
@@ -118,14 +119,17 @@ const SettingMyProfile = () => {
             </section>
             <section className="flex justify-between">
               <span className="font-title-sub-r text-gray-600">로그아웃</span>
-              <button className="w-[12.8rem] px-[1.6rem] py-[0.8rem] justify-center items-center rounded-md bg-gray-200 font-small-r text-[#F00] cursor-pointer">
+              <button
+                className="w-[12.8rem] px-[1.6rem] py-[0.8rem] justify-center items-center rounded-md bg-gray-200 font-small-r text-[#F00] cursor-pointer"
+                onClick={() => openModal({ name: 'logout' })}
+              >
                 로그아웃
               </button>
             </section>
           </div>
         </div>
       </div>
-      {isOpen && content && (
+      {isOpen && content?.name === 'withdraw' && (
         <WithdrawModal
           title="정말로 탈퇴하시겠어요?"
           subtitle="탈퇴 시 모든 데이터가 즉시 삭제되며, 복구가 불가능합니다."
@@ -144,6 +148,15 @@ const SettingMyProfile = () => {
           }
           disabled={!isAgree}
           onClose={() => setIsAgree(false)}
+        />
+      )}
+      {isOpen && content?.name === 'logout' && (
+        <Modal
+          title="로그아웃"
+          subtitle="정말로 로그아웃하시겠습니까?"
+          buttonText="로그아웃"
+          buttonColor="bg-error-400"
+          onClick={() => console.log('Modal Button Clicked')}
         />
       )}
     </div>
