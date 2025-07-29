25998684ddced5282035bcdbc54882f55b7d9893
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-07-30 01:17:06 +0900

    #80 [FEAT] 알림 리스트 api

---
+++ src/apis/alarm.ts
@@ -0,0 +1,20 @@
+import type { ResponseAlarmDto } from '../types/alarm';
+import type { PaginationDto } from '../types/common';
+import type { AlarmType } from '../types/listItem';
+import { axiosInstance } from './axios';
+
+export const getAlarmList = async (
+  alarmtype: AlarmType,
+  paginationDto: PaginationDto
+): Promise<ResponseAlarmDto> => {
+  try {
+    const { query } = paginationDto;
+    const { data } = await axiosInstance.get(`api/alarms/${alarmtype}`, {
+      params: { query },
+    });
+    return data;
+  } catch (error) {
+    console.error('Error fetching alarm list:', error);
+    throw error;
+  }
+};
