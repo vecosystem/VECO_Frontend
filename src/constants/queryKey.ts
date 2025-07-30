afcc9edd052d9045a11d447a64e3cd4160e2cac1
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-07-30 14:10:52 +0900

    #80 [FEAT] 쿼리키 상수 추가

--- src/constants/queryKey.ts
+++ src/constants/queryKey.ts
@@ -2,4 +2,8 @@ export const queryKey = {
   WORKSPACE_TEAMS: 'workspace_teams',
   WORKSPACE_MEMBERS: 'workspace_members',
   GITHUB_LINK: 'github_link',
+  GOAL_LIST: 'goal_list',
+  ISSUE_LIST: 'issue_list',
+  EXTERNAL_LIST: 'external_list',
+  NOTI_LIST: 'noti_list',
 };
