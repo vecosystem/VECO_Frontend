74236c872f91bd4f2c0a0890fb635fbabebf2329
waldls <p353700@gmail.com>
2025-07-31 23:31:34 +0900

    #104 [FEAT] 워크스페이스 URL 생성 요청 함수 구현

--- src/apis/workspace/workspace.ts
+++ src/apis/workspace/workspace.ts
@@ -1,5 +1,10 @@
 import { axiosInstance } from '../axios.ts';
-import type { CreateWorkspaceRequest, CreateWorkspaceResponse } from '../../types/workspace.ts';
+import type {
+  CreateWorkspaceRequest,
+  CreateWorkspaceResponse,
+  CreateWorkspaceUrlRequest,
+  CreateWorkspaceUrlResponse,
+} from '../../types/workspace.ts';
 
 /**
  * 워크스페이스 생성 요청 함수
@@ -8,5 +13,18 @@ export const createWorkspace = async (
   data: CreateWorkspaceRequest
 ): Promise<CreateWorkspaceResponse> => {
   const response = await axiosInstance.post<CreateWorkspaceResponse>('/api/workspace', data);
+  return response.data;
+};
+
+/**
+ * 워크스페이스 URL 생성 요청 함수
+ */
+export const createWorkspaceUrl = async (
+  data: CreateWorkspaceUrlRequest
+): Promise<CreateWorkspaceUrlResponse> => {
+  const response = await axiosInstance.post<CreateWorkspaceUrlResponse>(
+    '/api/workspace/create-url',
+    data
+  );
   return response.data;
 };
