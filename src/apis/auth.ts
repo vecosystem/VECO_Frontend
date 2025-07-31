e142083806f1204a99e586c58f8855903d4bc8b4
waldls <p353700@gmail.com>
2025-08-01 01:44:49 +0900

    #104 [FIX] 잘못된 엔드포인트 수정

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -7,7 +7,7 @@ import type { ReIssueTokenResponse } from '../types/auth';
  */
 export const postReIssueAccessToken = async (): Promise<string> => {
   const { data }: { data: ReIssueTokenResponse } = await axiosInstance.post(
-    '/token/reissue',
+    'api/token/reissue',
     null,
     {
       withCredentials: true, // 쿠키 포함 (refreshToken 전송)
