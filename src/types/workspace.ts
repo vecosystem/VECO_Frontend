5e7845e258451ed9d5240322f6546234063b57c1
waldls <p353700@gmail.com>
2025-07-31 23:05:34 +0900

    #104 [FEAT] 워크스페이스 & URL 생성 type 구현

---
+++ src/types/workspace.ts
@@ -0,0 +1,23 @@
+import type { CommonResponse } from './common';
+
+// 워크스페이스 생성
+export interface CreateWorkspaceRequest {
+  workspaceName: string;
+}
+
+export type CreateWorkspaceResponse = CommonResponse<{
+  workspaceId: number;
+  workspaceName: string;
+  inviteUrl: string;
+  invitePassWord: string;
+  defaultTeamId: number;
+}>;
+
+// 워크스페이스 URL 생성
+export interface CreateWorkspaceUrlRequest {
+  workspaceName: string;
+}
+
+export type CreateWorkspaceUrlResponse = CommonResponse<{
+  workspaceUrl: string;
+}>;
