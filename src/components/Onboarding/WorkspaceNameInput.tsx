83f1ed72097a61a800f5ec09218a8a86c15a5d1a
waldls <p353700@gmail.com>
2025-08-01 12:13:57 +0900

    #104 [CHORE] 체크 버튼에 cursor-pointer 속성 추가

--- src/components/Onboarding/WorkspaceNameInput.tsx
+++ src/components/Onboarding/WorkspaceNameInput.tsx
@@ -66,7 +66,7 @@ const WorkspaceNameInput = ({ onUrlGenerated }: WorkspaceNameInputProps) => {
           <button
             type="button"
             onClick={handleCheck}
-            className="w-[4rem] h-[3.6rem] flex items-center justify-center rounded-[0.6rem] bg-primary-blue ml-[1.3rem] shrink-0"
+            className="w-[4rem] h-[3.6rem] flex items-center justify-center rounded-[0.6rem] bg-primary-blue ml-[1.3rem] shrink-0 cursor-pointer"
           >
             <img src={whitecheck} alt="Check" className="w-[1.6rem] h-[1.6rem]" />
           </button>
