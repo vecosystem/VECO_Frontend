05c3e8cff0db9f9532142b84aaac57fc2f2260a4
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-08-01 20:10:59 +0900

    #80 [MOVE] 목표 api 파일이동

--- src/apis/goal/useGetGoalList.ts
+++ src/apis/goal/useGetGoalList.ts
@@ -1,7 +1,25 @@
 import { useQuery } from '@tanstack/react-query';
 import { queryKey } from '../../constants/queryKey';
-import { getGoalList } from './goal';
 import type { PaginationDto } from '../../types/common';
+import type { RequestGoalListDto, ResponseGoalDto } from '../../types/goal';
+import { axiosInstance } from '../axios';
+
+// goal/GoalHome.tsx
+const getGoalList = async (
+  { teamId }: RequestGoalListDto,
+  paginationDto: PaginationDto
+): Promise<ResponseGoalDto> => {
+  try {
+    const { data } = await axiosInstance.get(`/api/teams/${teamId}/goals`, {
+      params: paginationDto,
+    });
+
+    return data;
+  } catch (error) {
+    console.error('Error fetching goal list:', error);
+    throw error;
+  }
+};
 
 export const useGetGoalList = (teamId: string, params: PaginationDto) => {
   return useQuery({
