fe1b1fe6462c9f80dd53824a77f8e5b76efebec0
waldls <p353700@gmail.com>
2025-08-02 21:45:17 +0900

    #114 [MOD] vite-env.d.ts 수정

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,6 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   readonly VITE_SERVER_API_URL: string;
-  readonly VITE_SLACK_CLIENT_ID: string;
-  readonly VITE_SLACK_REDIRECT_URI: string;
 }
 
 interface ImportMeta {
