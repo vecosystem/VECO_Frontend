ec4f0687938ee8842be7a6ce12a6d3bff8ed9f2b
waldls <p353700@gmail.com>
2025-08-02 13:44:51 +0900

    #114 [MOD] 기본 axios 인스턴스 쿠키 포함

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -6,13 +6,7 @@ import type { ReIssueTokenResponse } from '../types/auth';
  * - pages/onboarding/GoogleLoginRedirect.tsx
  */
 export const postReIssueAccessToken = async (): Promise<string> => {
-  const { data }: { data: ReIssueTokenResponse } = await axiosInstance.post(
-    '/api/token/reissue',
-    null,
-    {
-      withCredentials: true, // 쿠키 포함 (refreshToken 전송)
-    }
-  );
+  const { data }: { data: ReIssueTokenResponse } = await axiosInstance.post('/api/token/reissue');
 
   if (!data.result?.accessToken) {
     throw new Error('accessToken 재발급 실패');
