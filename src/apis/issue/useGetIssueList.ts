adfb521c8357f9be409a5f38f553a7de2bbb305b
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-08-05 01:13:34 +0900

    #119 [FEAT] 이슈 무한스크롤 훅

--- src/apis/issue/useGetIssueList.ts
+++ src/apis/issue/useGetIssueList.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import { queryKey } from '../../constants/queryKey';
 import type { PaginationDto } from '../../types/common';
 import type { RequestIssueListDto, ResponseIssueDto } from '../../types/issue';
@@ -25,5 +25,25 @@ export const useGetIssueList = (teamId: string, params: PaginationDto) => {
   return useQuery({
     queryKey: [queryKey.ISSUE_LIST, teamId, params],
     queryFn: () => getIssueList({ teamId }, params),
+  });
+};
+
+export const useGetInfiniteIssueList = (teamId: string, params: PaginationDto) => {
+  return useInfiniteQuery({
+    queryKey: [queryKey.ISSUE_LIST, teamId, params.query],
+    queryFn: ({ pageParam = '0' }) =>
+      getIssueList({ teamId }, { ...params, cursor: pageParam, size: 3 }), // 한 번에 불러올 데이터 개수
+    initialPageParam: '-1',
+    getNextPageParam: (lastPage: ResponseIssueDto) => {
+      if (lastPage.result?.hasNext) {
+        return lastPage.result.nextCursor;
+      }
+      return undefined;
+    },
+    select: (data) => ({
+      pages: data.pages.flatMap((page) => page.result?.data ?? []),
+      pageParams: data.pageParams,
+    }),
+    enabled: !!teamId,
   });
 };
