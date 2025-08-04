c29a3df085da6956afa9281b5b4c1d64a9baa2f0
Lee Gaeul <91470334+gaeulzzang@users.noreply.github.com>
2025-08-04 20:52:37 +0900

    #126 [UI] 에러 페이지 구현

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -5,7 +5,7 @@ const Error = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col w-full items-center justify-center bg-gray-onboard">
+    <div className="flex flex-col w-full h-dvh items-center justify-center bg-gray-onboard">
       <div className="h-[42rem] flex flex-col justify-end">
         <div className="flex flex-col items-center gap-[10.8rem]">
           {/* 에러 문구 */}
@@ -17,7 +17,13 @@ const Error = () => {
             </h3>
           </div>
           {/* 돌아가기 버튼 */}
-          <PrimaryButton text="돌아가기" onClick={() => navigate(-1)} />
+          <PrimaryButton
+            text="돌아가기"
+            onClick={() => {
+              navigate(-1);
+              window.location.reload();
+            }}
+          />
         </div>
       </div>
     </div>
