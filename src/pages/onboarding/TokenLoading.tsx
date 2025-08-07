569693a01e4aeb62aee26dccd5051fe2249da2e6
waldls <p353700@gmail.com>
2025-08-08 00:58:20 +0900

    #140 [DEL] 주석 삭제

--- src/pages/onboarding/TokenLoading.tsx
+++ src/pages/onboarding/TokenLoading.tsx
@@ -12,37 +12,24 @@ const TokenLoading = () => {
   useEffect(() => {
     const init = async () => {
       try {
-        console.log('[TokenLoading] 시작됨');
-
         const accessToken = await postReIssueAccessToken();
         setItem(accessToken);
-        console.log('[TokenLoading] ✅ accessToken 재발급 성공');
-
-        // 1. 워크스페이스 정보 조회
         try {
           await getWorkspaceProfile();
-          console.log('[TokenLoading] ✅ 워크스페이스 존재 → /workspace');
           navigate('/workspace');
         } catch (err: any) {
           if (err.response?.status === 404) {
-            console.log('[TokenLoading] ❌ 워크스페이스 없음 (404)');
-
             const isInvite = localStorage.getItem('isInvite');
-            console.log('[TokenLoading] 🔍 isInvite:', isInvite);
-
             if (isInvite === 'true') {
-              console.log('[TokenLoading] 🔁 초대 사용자 → /onboarding/input-pw');
               navigate('/onboarding/input-pw');
             } else {
-              console.warn('[TokenLoading] ⚠️ 일반 사용자 → /onboarding/workspace');
               navigate('/onboarding/workspace');
             }
           } else {
-            console.error('[TokenLoading] ❌ 워크스페이스 조회 실패:', err);
+            console.log('[TokenLoading] ❌ 워크스페이스 조회 실패:', err);
           }
         }
       } catch (err) {
-        console.error('[TokenLoading] ❌ accessToken 재발급 실패:', err);
         navigate('/onboarding'); // fallback
       }
     };
