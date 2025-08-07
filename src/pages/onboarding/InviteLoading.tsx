1f287f4acdfe505026a5004ac6e9e999866eb92f
waldls <p353700@gmail.com>
2025-08-08 00:13:54 +0900

    #140 [FEAT] 초대된 사용자 리다이렉트 페이지 구현

--- src/pages/onboarding/InviteLoading.tsx
+++ src/pages/onboarding/InviteLoading.tsx
@@ -1,5 +1,23 @@
+// src/pages/onboarding/InviteLoading.tsx
+import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
 const InviteLoading = () => {
-  return <div>내부로직페이지1</div>;
+  const { workspaceName } = useParams(); // 1. URL 파라미터 추출
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (workspaceName) {
+      // 2. 로컬스토리지에 저장
+      localStorage.setItem('workspaceName', workspaceName);
+      localStorage.setItem('isInvite', 'true');
+
+      // 3. /onboarding으로 이동
+      navigate('/onboarding');
+    }
+  }, [workspaceName, navigate]);
+
+  return <div>초대 경로 확인 중입니다...</div>;
 };
 
 export default InviteLoading;
