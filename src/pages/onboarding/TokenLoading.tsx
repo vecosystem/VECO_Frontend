b70ef82046332f1e4ee08ea6ced64741ba504b49
waldls <p353700@gmail.com>
2025-08-07 16:17:37 +0900

    [FEAT] TokenLoading 페이지에서 accessToken 발급

--- src/pages/onboarding/TokenLoading.tsx
+++ src/pages/onboarding/TokenLoading.tsx
@@ -1,5 +1,21 @@
+import { useEffect } from 'react';
+import { postReIssueAccessToken } from '../../apis/auth';
+import { LOCAL_STORAGE_KEY } from '../../constants/key';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
+
 const TokenLoading = () => {
-  return <div>TokenLoading</div>;
+  const { setItem } = useLocalStorage(LOCAL_STORAGE_KEY.accessToken);
+
+  useEffect(() => {
+    const fetchAccessToken = async () => {
+      const accesstoken = await postReIssueAccessToken();
+      setItem(accesstoken);
+      console.log(accesstoken);
+    };
+    fetchAccessToken();
+  }, []);
+
+  return <h3 className="font-title-sub-r text-gray-600">로딩중입니다.</h3>;
 };
 
 export default TokenLoading;
