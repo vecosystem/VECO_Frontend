c652d85112889e19116653befe1bb4d6b043c33e
waldls <p353700@gmail.com>
2025-08-08 14:43:44 +0900

    #140 [MOD] 원상 복구

--- src/routes/ProtectedRoutes.tsx
+++ src/routes/ProtectedRoutes.tsx
@@ -1,81 +1,80 @@
-import { Navigate, Outlet, type RouteObject } from 'react-router-dom';
-import ProtectedLayout from '../layouts/ProtectedLayout';
-import GoalHome from '../pages/goal/GoalHome';
-import IssueHome from '../pages/issue/IssueHome';
-import NotiHome from '../pages/notification/NotiHome';
+import type { RouteObject } from 'react-router-dom';
+import AuthRedirect from '../components/AuthRedirect';
+import PublicLayout from '../layouts/PublicLayout';
+import OnboardingSSOLogin from '../pages/onboarding/OnboardingSSOLogin';
+import OnboardingCreateWorkspace from '../pages/onboarding/OnboardingCreateWorkspace';
+import OnboardingInviteMember from '../pages/onboarding/OnboardingInviteMember';
+import OnboardingFinish from '../pages/onboarding/OnboardingFinish';
 import Error404NotFound from '../pages/Error404NotFound';
-import SettingTeam from '../pages/setting/SettingTeam.tsx';
-import SettingMember from '../pages/setting/SettingMember.tsx';
-import SettingMyProfile from '../pages/setting/SettingMyProfile.tsx';
-import ExternalHome from '../pages/external/ExternalHome.tsx';
-import WorkspaceIssue from '../pages/workspace/WorkspaceIssue.tsx';
-import WorkspaceGoal from '../pages/workspace/WorkspaceGoal.tsx';
-import WorkspaceExternal from '../pages/workspace/WorkspaceExternal.tsx';
-import GoalDetail from '../pages/goal/GoalDetail.tsx';
-import IssueDetail from '../pages/issue/IssueDetail.tsx';
-import SettingWorkspaceProfile from '../pages/setting/SettingWorkspaceProfile.tsx';
-import ExternalDetail from '../pages/external/ExternalDetail.tsx';
-import WorkspaceGoalDetail from '../pages/workspace/WorkspaceGoalDetail.tsx';
-import WorkspaceIssueDetail from '../pages/workspace/WorkspaceIssueDetail.tsx';
-import WorkspaceExternalDetail from '../pages/workspace/WorkspaceExternalDetail.tsx';
+import ParticipateWorkspaceInputPw from '../pages/onboarding/ParticipateWorkspaceInputPw';
+import { HomePage } from '../pages/home/HomePage.tsx';
+import TokenLoading from '../pages/onboarding/TokenLoading.tsx';
+import InviteLoading from '../pages/onboarding/InviteLoading.tsx';
 
-export const protectedRoutes: RouteObject[] = [
+export const publicRoutes: RouteObject[] = [
   {
-    // 워크스페이스 내부 페이지들 : 로그인해야 들어올 수 있음
-    path: '/workspace',
-    element: <ProtectedLayout />,
+    path: '/',
+    element: <HomePage />,
     errorElement: <Error404NotFound />,
-
     children: [
-      { index: true, element: <WorkspaceIssue /> },
-      /* 알람 페이지 */
+      // 도메인 최상위: 백호 랜딩페이지
+      { index: true, element: <HomePage /> },
+      // '시작하기'를 눌렀을 때 AuthRedirect 컴포넌트로 리다이렉트.
+      // 사용자 인증 여부에 따라 protected 또는 public 경로로 리다이렉트됨.
+      { path: 'entry', element: <AuthRedirect /> },
+    ],
+  },
+  /* 초대 받은 사용자를 위한 리다이렉트 페이지 */
+  {
+    path: '/:workspaceName/invite',
+    element: <InviteLoading />,
+  },
+  /* Onboarding 단계 페이지들 */
+  {
+    path: '/onboarding',
+    element: <PublicLayout />,
+    errorElement: <Error404NotFound />,
+    children: [
       {
-        path: 'noti',
-        element: <Outlet />,
-        children: [{ index: true, element: <NotiHome /> }],
+        index: true,
+        element: (
+          <div>
+            <OnboardingSSOLogin />
+          </div>
+        ),
       },
-      /* 설정 페이지들 */
       {
-        path: 'setting',
-        element: <Outlet />,
-        children: [
-          // 기본 경로는 워크스페이스 프로필 페이지로 리다이렉트.
-          { index: true, element: <Navigate to="ws-profile" replace /> },
-          { path: 'ws-profile', element: <SettingWorkspaceProfile /> },
-          { path: 'team-list', element: <SettingTeam /> },
-          { path: 'team-members', element: <SettingMember /> },
-          { path: 'my-profile', element: <SettingMyProfile /> },
-        ],
+        path: 'workspace',
+        element: (
+          <div>
+            <OnboardingCreateWorkspace />
+          </div>
+        ),
       },
-      /* 워크스페이스 전체 팀 페이지들 */
       {
-        path: 'default/team/:teamId',
-        element: <Outlet />,
-        children: [
-          // 기본 경로는 이슈 페이지로 리다이렉트
-          { index: true, element: <Navigate to="issue" replace /> },
-          { path: 'issue', element: <WorkspaceIssue /> },
-          { path: 'issue/:issueId', element: <WorkspaceIssueDetail /> },
-          { path: 'goal', element: <WorkspaceGoal /> },
-          { path: 'goal/:goalId', element: <WorkspaceGoalDetail /> },
-          { path: 'ext', element: <WorkspaceExternal /> },
-          { path: 'ext/:extId', element: <WorkspaceExternalDetail /> },
-        ],
+        path: 'invite',
+        element: (
+          <div>
+            <OnboardingInviteMember />
+          </div>
+        ),
       },
-      /* 팀별 페이지들 */
       {
-        path: 'team/:teamId',
-        element: <Outlet />,
-        children: [
-          // 기본 경로는 이슈 페이지로 리다이렉트.
-          { index: true, element: <Navigate to="issue" replace /> },
-          { path: 'goal', element: <GoalHome /> },
-          { path: 'goal/:goalId', element: <GoalDetail /> },
-          { path: 'issue', element: <IssueHome /> },
-          { path: 'issue/:issueId', element: <IssueDetail /> },
-          { path: 'ext', element: <ExternalHome /> },
-          { path: 'ext/:extId', element: <ExternalDetail /> },
-        ],
+        path: 'fin',
+        element: (
+          <div>
+            <OnboardingFinish />
+          </div>
+        ),
+      },
+      {
+        path: 'input-pw',
+        element: <ParticipateWorkspaceInputPw />,
+      },
+      /* accessToken 저장 및 워크스페이스 조회하는 리다이렉트 페이지 */
+      {
+        path: 'loading',
+        element: <TokenLoading />,
       },
     ],
   },
