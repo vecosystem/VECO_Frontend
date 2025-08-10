770d3efc9916698357cdb6ed51a8773947877767
waldls <p353700@gmail.com>
2025-08-10 22:31:05 +0900

    #124 [REFACTOR] WorkspaceComplete 페이지 기본 스타일링 수정

--- src/pages/workspace/WorkspaceComplete.tsx
+++ src/pages/workspace/WorkspaceComplete.tsx
@@ -1,5 +1,11 @@
 const WorkspaceComplete = () => {
-  return <div>WorkspaceComplete</div>;
+  return (
+    <div className="min-w-max min-h-screen flex flex-col items-center justify-center">
+      <h3 className="font-title-sub-r text-gray-600">
+        워크스페이스 생성 및 참여가 완료되었습니다.
+      </h3>
+    </div>
+  );
 };
 
 export default WorkspaceComplete;
