8b5701368b9dafd4147f28029f88f47afb8096f5
waldls <p353700@gmail.com>
2025-08-10 18:55:49 +0900

    #124 [FEAT] 워크스페이스 참여 API 연동

--- src/pages/onboarding/ParticipateWorkspaceInputPw.tsx
+++ src/pages/onboarding/ParticipateWorkspaceInputPw.tsx
@@ -6,23 +6,36 @@ import vecocirclenavy from '../../assets/logos/veco-circle-logo-bg-navy.svg';
 import InviteCodeInput from '../../components/Onboarding/InviteCodeInput';
 import PrimaryButton from '../../components/Onboarding/PrimaryButton';
 import { LOCAL_STORAGE_KEY } from '../../constants/key';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
+import { usePostJoinWorkspace } from '../../apis/workspace/usePostJoinWorkspace';
 
 const ParticipateWorkspaceInputPw = () => {
   const [inputCode, setInputCode] = useState(''); // 사용자 입력 코드 상태
   const [hasError, setHasError] = useState(false); // 사용자 입력 코드 에러 상태
   const navigate = useNavigate(); // 페이지 이동을 위한 훅
-  const correctCode = 'tempcode'; // 올바른 초대 코드 (백엔드 연동 전 임시 하드코딩)
+
+  const { getItem: getInviteToken } = useLocalStorage(LOCAL_STORAGE_KEY.token);
+  const { getItem: getWorkspaceName } = useLocalStorage(LOCAL_STORAGE_KEY.workspaceName);
+
+  const { mutate, isPending, isError } = usePostJoinWorkspace();
 
   // 입장하기 버튼 클릭 시 처리 로직
   const handleClick = () => {
-    if (inputCode === correctCode) {
-      // 옳은 코드 - 에러 제거 및 워크스페이스 홈으로 이동
-      setHasError(false);
-      navigate('/workspace/default/team/:teamId');
-    } else {
-      // 잘못된 코드 - 에러 표시
-      setHasError(true);
-    }
+    mutate(
+      {
+        token: getInviteToken() || '',
+        password: inputCode,
+      },
+      {
+        onSuccess: () => {
+          setHasError(false);
+          navigate('/workspace'); // TODO: 실제 팀 경로로 교체
+        },
+        onError: () => {
+          setHasError(true);
+        },
+      }
+    );
   };
 
   return (
@@ -32,7 +45,7 @@ const ParticipateWorkspaceInputPw = () => {
         <img src={vecocirclenavy} alt="Veco" className="w-[10rem] h-[10rem]" />
         {/* 초대 문구 */}
         <h2 className="font-title-b text-gray-600">
-          {localStorage.getItem(LOCAL_STORAGE_KEY.workspaceName)} 워크스페이스에 초대합니다.
+          {getWorkspaceName()} 워크스페이스에 초대합니다.
         </h2>
         {/* 암호 입력란 - 백엔드 연동 후 수정 */}
         <InviteCodeInput inputCode={inputCode} onChange={setInputCode} hasError={hasError} />
