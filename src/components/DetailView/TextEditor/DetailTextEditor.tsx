13f2206a813bac6c8c469cb111740eaf1eb4b2ed
HiJuwon <wizz2176ytr@naver.com>
2025-08-13 01:56:13 +0900

    #149 [FEAT] lexical to JSON 직렬화 로직 구현

--- src/components/DetailView/TextEditor/DetailTextEditor.tsx
+++ src/components/DetailView/TextEditor/DetailTextEditor.tsx
@@ -1,10 +1,5 @@
-/**
- * DetailTextEditor.tsx
- * 상세페이지 설명 작성 컴포넌트
- *
- * @todo
- * - onChange로 외부 state 연동 (@lexical/react/LexicalOnChangePlugin 붙여서 JSON으로 내보내면 저장/요청 api 연동 가능)
- */
+// DetailTextEditor.tsx
+// 상세페이지 설명 작성 컴포넌트
 
 import { type ComponentProps } from 'react';
 import { LexicalComposer } from '@lexical/react/LexicalComposer';
@@ -22,10 +17,13 @@ import { CodeHighlightPlugin } from './lexical-plugins/CodeHighlightPlugin';
 import { theme } from './editor-theme/editorTheme';
 import IndentPlugin from './lexical-plugins/IndentPlugin';
 import EnterAsParagraphPlugin from './lexical-plugins/EnterAsParagraphPlugin';
+import type { SubmitHandleRef } from './lexical-plugins/SubmitHandlePlugin';
+import SubmitHandlePlugin from './lexical-plugins/SubmitHandlePlugin';
 
 // 부모 컴포넌트에서 전달하는 props
 interface DetailTextEditorProps {
   isEditable: boolean; // 편집가능 여부 - true일 때만 상세 설명 내용 입력 가능.
+  submitRef: React.RefObject<SubmitHandleRef | null>;
 }
 
 // initialConfig: LexicalEditor의 초기 설정 (필수 요소)
@@ -34,9 +32,12 @@ const initialConfig: ComponentProps<typeof LexicalComposer>['initialConfig'] = {
   onError: (e) => console.error(e),
   nodes: lexicalnodes, // Lexical에서 사용할 커스텀 노드 클래스 목록을 전달.
   theme: theme,
+
+  // TODO: 수정 모드나 조회 모드에서 받은 content JSON을 렌더링하기 위한 초기 설정 (추후 API 연동시 반영)
+  // editorState: (editor) => safeLoadEditorState(editor, initialContentJson),
 };
 
-const DetailTextEditor = ({ isEditable }: DetailTextEditorProps) => {
+const DetailTextEditor = ({ isEditable, submitRef }: DetailTextEditorProps) => {
   return (
     <div className="w-full h-full flex-1 overflow-y-scroll basic-scroll resize-none">
       {/* LexicalComposer: LexicalEditor 인스턴스 생성 -> Context.Provider를 통해 전달 */}
@@ -54,6 +55,7 @@ const DetailTextEditor = ({ isEditable }: DetailTextEditorProps) => {
           <CodeHighlightPlugin />
           <IndentPlugin />
           <EnterAsParagraphPlugin />
+          <SubmitHandlePlugin ref={submitRef} /> {/* 부모의 ref 그대로 전달 */}
           {/* 텍스트에디터 본문 Contents */}
           <div className="relative flex-1 editor-root group">
             <RichTextPlugin
