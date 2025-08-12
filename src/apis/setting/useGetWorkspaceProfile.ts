91a6d6d096f7c7c25608c308dc568b9892d5dbbb
Lee Gaeul <91470334+gaeulzzang@users.noreply.github.com>
2025-08-12 22:53:26 +0900

    #153 [FEAT] 로그인 시 워크스페이스 정보 로컬 스토리지에 저장

--- src/apis/setting/useGetWorkspaceProfile.ts
+++ src/apis/setting/useGetWorkspaceProfile.ts
@@ -2,6 +2,7 @@ import type { WorkspaceResponse } from '../../types/setting';
 import { axiosInstance } from '../axios';
 import { useQuery } from '@tanstack/react-query';
 import { queryKey } from '../../constants/queryKey';
+import { LOCAL_STORAGE_KEY } from '../../constants/key.ts';
 
 // 설정 - 워크스페이스 프로필 조회
 // - src/pages/onboarding/TokenLoading.tsx
@@ -19,5 +20,16 @@ export const useGetWorkspaceProfile = () => {
   return useQuery({
     queryKey: [queryKey.WORKSPACE_PROFILE],
     queryFn: getWorkspaceProfile,
+    select: (data) => {
+      const inviteUrl = localStorage.getItem(LOCAL_STORAGE_KEY.inviteUrl);
+      const invitePassword = localStorage.getItem(LOCAL_STORAGE_KEY.invitePassword);
+      if (!inviteUrl) {
+        localStorage.setItem(LOCAL_STORAGE_KEY.inviteUrl, data.workspaceUrl);
+      }
+      if (!invitePassword) {
+        localStorage.setItem(LOCAL_STORAGE_KEY.invitePassword, data.invitePassword);
+      }
+      return data;
+    },
   });
 };
