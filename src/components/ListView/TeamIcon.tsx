cee11957c631241ab71153a358996fa9b46d0a67
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-08-12 17:53:14 +0900

    #157 [FEAT] 팀 아이콘 onClick 요소 추가

--- src/components/ListView/TeamIcon.tsx
+++ src/components/ListView/TeamIcon.tsx
@@ -3,19 +3,20 @@ import logo from '../../assets/logos/veco-circle-logo-bg-white.svg';
 interface TeamIconProps {
   teamName?: string;
   teamImgUrl?: string;
+  onClick?: () => void;
 }
 
-const TeamIcon = ({ teamName, teamImgUrl }: TeamIconProps) => {
+const TeamIcon = ({ teamName, teamImgUrl, onClick }: TeamIconProps) => {
   return (
-    <>
-      <div className="flex gap-[1.6rem] items-center">
+    <div className="flex">
+      <div className="flex gap-[1.6rem] items-center cursor-pointer w-fit" onClick={onClick}>
         <span
           className="inline-block w-[3.2rem] h-[3.2rem] rounded-full bg-center bg-no-repeat bg-cover"
           style={{ backgroundImage: `url("${teamImgUrl || logo}")` }}
         />
         <div className="flex font-title-b">{teamName || 'Team'}</div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/components/ListView/WorkspaceIcon.tsx
+++ src/components/ListView/WorkspaceIcon.tsx
@@ -3,19 +3,20 @@ import logo from '../../assets/logos/veco-circle-logo-bg-navy.svg';
 interface WorkspaceIconProps {
   workspaceName?: string;
   workspaceImgUrl?: string;
+  onClick?: () => void;
 }
 
-const WorkspaceIcon = ({ workspaceName, workspaceImgUrl }: WorkspaceIconProps) => {
+const WorkspaceIcon = ({ workspaceName, workspaceImgUrl, onClick }: WorkspaceIconProps) => {
   return (
-    <>
-      <div className="flex gap-[1.6rem] items-center">
+    <div className="flex">
+      <div className="flex gap-[1.6rem] items-center cursor-pointer w-fit" onClick={onClick}>
         <span
           className="inline-block w-[3.2rem] h-[3.2rem] rounded-full bg-center bg-no-repeat bg-cover"
           style={{ backgroundImage: `url("${workspaceImgUrl || logo}")` }}
         />
         <div className="flex font-title-b">{workspaceName || 'Workspace'}</div>
       </div>
-    </>
+    </div>
   );
 };
 
