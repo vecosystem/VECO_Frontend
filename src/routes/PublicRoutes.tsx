fae23ea6baf052beb60296641b88044c5f9bb4bd
waldls <p353700@gmail.com>
2025-08-14 21:42:59 +0900

    #158 [MOD] AuthRedirct 연결 해제

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -1,5 +1,4 @@
 import type { RouteObject } from 'react-router-dom';
-import AuthRedirect from '../components/AuthRedirect';
 import PublicLayout from '../layouts/PublicLayout';
 import OnboardingSSOLogin from '../pages/onboarding/OnboardingSSOLogin';
 import OnboardingCreateWorkspace from '../pages/onboarding/OnboardingCreateWorkspace';
@@ -21,9 +20,6 @@ export const publicRoutes: RouteObject[] = [
     children: [
       // 도메인 최상위: 백호 랜딩페이지
       { index: true, element: <HomePage /> },
-      // '시작하기'를 눌렀을 때 AuthRedirect 컴포넌트로 리다이렉트.
-      // 사용자 인증 여부에 따라 protected 또는 public 경로로 리다이렉트됨.
-      { path: 'entry', element: <AuthRedirect /> },
     ],
   },
   /* 초대 받은 사용자를 위한 리다이렉트 페이지 */
