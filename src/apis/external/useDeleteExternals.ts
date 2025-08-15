ac8bef6e13b617ab7eb1be9e2f326649f85b73ae
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-08-15 10:53:22 +0900

    #157 [FEAT] 외부 목록 삭제 mutation

--- src/apis/external/useDeleteExternals.ts
+++ src/apis/external/useDeleteExternals.ts
@@ -1,11 +1,7 @@
 import { useMutation } from '@tanstack/react-query';
 import queryClient from '../../utils/queryClient';
 import { queryKey } from '../../constants/queryKey';
-import type {
-  ExternalFilter,
-  RequestExternalListDto,
-  ResponseExternalDto,
-} from '../../types/external';
+import type { RequestExternalListDto } from '../../types/external';
 import type { CommonResponse } from '../../types/common';
 import { axiosInstance } from '../axios';
 
@@ -29,53 +25,14 @@ export const deleteExternalItem = async ({
 export const useDeleteExternals = () => {
   return useMutation({
     mutationFn: deleteExternalItem,
-
-    onMutate: async (variables) => {
-      await queryClient.cancelQueries({
+    onSuccess(data, variables) {
+      console.log('Externals deleted successfully:', data);
+      queryClient.invalidateQueries({
         queryKey: [queryKey.EXTERNAL_LIST, variables.teamId],
       });
-
-      const prevGoals = queryClient.getQueryData<ResponseExternalDto>([
-        queryKey.GOAL_LIST,
-        variables.teamId,
-      ]);
-
-      if (prevGoals?.result?.data) {
-        const optimisticGoals = {
-          ...prevGoals,
-          result: {
-            ...prevGoals.result,
-            data: prevGoals.result.data.map((filter: ExternalFilter) => ({
-              ...filter,
-              goals: filter.externals.filter(
-                (external) => !(variables.externalIds ?? []).includes(external.id)
-              ),
-              dataCnt: filter.externals.filter(
-                (external) => !(variables.externalIds ?? []).includes(external.id)
-              ).length,
-            })),
-          },
-        };
-
-        queryClient.setQueryData([queryKey.GOAL_LIST, variables.teamId], optimisticGoals);
-      }
-
-      return { prevGoals };
     },
-
-    // 에러 발생 시 롤백
-    onError: (err, variables, context?: { prevGoals?: ResponseExternalDto }) => {
-      if (context?.prevGoals) {
-        queryClient.setQueryData([queryKey.EXTERNAL_LIST, variables.teamId], context.prevGoals);
-      }
-      console.error('Error deleting external item:', err);
-    },
-
-    // 성공 시 서버 데이터로 동기화
-    onSettled: (_data, _error, variables) => {
-      queryClient.invalidateQueries({
-        queryKey: [queryKey.GOAL_LIST, variables.teamId],
-      });
+    onError(error) {
+      console.error('Error deleting external item:', error);
     },
   });
 };
