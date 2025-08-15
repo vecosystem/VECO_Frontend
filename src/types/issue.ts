b559a8a62da7e4f78f11dd85ea2b7d63b8c0a688
HiJuwon <wizz2176ytr@naver.com>
2025-08-15 18:46:16 +0900

    #172 [FEAT] 이슈 타입 구조 정의 완료

--- src/types/issue.ts
+++ src/types/issue.ts
@@ -1,4 +1,6 @@
+import type { ResponseCommentListDto } from './comment';
 import type { CommonResponse, CursorBasedResponse } from './common';
+import type { SimpleGoal } from './goal';
 
 /* 임시 구조, api 명세서 완성 시 변경 필요하면 수정 예정 */
 
@@ -63,7 +65,7 @@ export type CreateIssueDetailDto = {
   priority: string;
   managersId: number[];
   deadline?: Deadline;
-  goalId: number; // 이거 필수 요소인지 확인
+  goalId: number; // 이거 필수 요소인지 서버에 확인
 };
 
 export type CreateIssueResultDto = {
@@ -82,6 +84,28 @@ export type ViewIssueDetailDto = {
   state: string;
   priority: string;
   deadline: Deadline;
+  goal: SimpleGoal;
+  managers: Manager;
+  comments: ResponseCommentListDto;
 };
 
+export type ResponseViewIssueDetailDto = CommonResponse<ViewIssueDetailDto>;
+
 // 이슈 수정
+export type UpdateIssueDetailDto = {
+  // 변경사항이 없는 속성은 Null값 가능, 키 생략(undef)도 가능
+  title?: string | null;
+  content?: string | null;
+  state?: string | null;
+  priority?: string | null;
+  managersId?: number[] | null;
+  deadline?: Deadline | null;
+  goalId: number | null;
+};
+
+export type UpdateIssueResultDto = {
+  issueId: number;
+  updatedAt: string; // LocalDateTime
+};
+
+export type ResponseUpdateIssueDetailDto = CommonResponse<UpdateIssueResultDto>;
