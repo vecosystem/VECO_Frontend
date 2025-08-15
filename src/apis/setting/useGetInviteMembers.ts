4e19839c500543a9f105e5b388b9c4f3cf650d17
waldls <p353700@gmail.com>
2025-08-15 10:54:43 +0900

    #176 [FEAT] 팀원 초대하기 함수 구현

---
+++ src/apis/setting/useGetInviteMembers.ts
@@ -0,0 +1,22 @@
+import type { InviteMemberResponse } from '../../types/setting.ts';
+import { axiosInstance } from '../axios.ts';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { queryKey } from '../../constants/queryKey.ts';
+
+// 설정 - 팀원 초대
+const getInviteMembers = async (): Promise<InviteMemberResponse> => {
+  try {
+    const response = await axiosInstance.get<InviteMemberResponse>('/api/workspace/setting/invite');
+    return response.data;
+  } catch (error) {
+    console.error('멤버 초대 목록 조회 실패', error);
+    throw error;
+  }
+};
+
+export const useGetInviteMembers = () => {
+  return useSuspenseQuery({
+    queryKey: [queryKey.WORKSPACE_MEMBERS],
+    queryFn: getInviteMembers,
+  });
+};
