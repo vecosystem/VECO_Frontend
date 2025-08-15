b293406ff53af4792c3f83303a955611b15f65ca
HiJuwon <wizz2176ytr@naver.com>
2025-08-15 17:54:01 +0900

    #172 [FEAT] 이슈 작성 api 타입 구조 정의

--- src/types/issue.ts
+++ src/types/issue.ts
@@ -1,10 +1,10 @@
-import type { CursorBasedResponse } from './common';
+import type { CommonResponse, CursorBasedResponse } from './common';
 
 /* 임시 구조, api 명세서 완성 시 변경 필요하면 수정 예정 */
 
 export type Deadline = {
-  start: string;
-  end: string;
+  start?: string;
+  end?: string;
 };
 
 export type Goal = {
@@ -54,3 +54,34 @@ export type SimpleIssueListDto = {
   cnt: number;
   info: SimpleIssue[];
 };
+
+// 이슈 작성
+export type CreateIssueDetailDto = {
+  title: string;
+  content: string;
+  state: string;
+  priority: string;
+  managersId: number[];
+  deadline?: Deadline;
+  goalId: number; // 이거 필수 요소인지 확인
+};
+
+export type CreateIssueResultDto = {
+  issueId: number;
+  createdAt: string;
+};
+
+export type ResponseCreateIssueDetailDto = CommonResponse<CreateIssueResultDto>;
+
+// 이슈 상세 조회
+export type ViewIssueDetailDto = {
+  id: number;
+  name: string;
+  title: string;
+  content: string;
+  state: string;
+  priority: string;
+  deadline: Deadline;
+};
+
+// 이슈 수정
