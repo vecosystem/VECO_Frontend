7bbc9ee3f74417d4c0b6123c3a440f0f0b29924a
HiJuwon <wizz2176ytr@naver.com>
2025-08-18 12:48:56 +0900

    #173 [FEAT] external.ts에 타입 구조 정의

--- src/types/external.ts
+++ src/types/external.ts
@@ -1,4 +1,6 @@
-import type { CursorBasedResponse } from './common';
+import type { ResponseCommentListDto } from './comment';
+import type { CommonResponse, CursorBasedResponse } from './common';
+import type { Goal } from './issue';
 
 export type Deadline = {
   start: string;
@@ -41,3 +43,64 @@ export type RequestExternalListDto = {
 };
 
 export type ResponseExternalDto = CursorBasedResponse<ExternalFilter[]>;
+
+// 외부 이슈 생성
+export type CreateExternalDetailDto = {
+  owner?: string;
+  repo?: string;
+  installationId?: number;
+  title: string;
+  content: string;
+  state: string;
+  priority: string;
+  managersId: number[];
+  deadline?: Deadline;
+  extServiceType: string;
+  goalId?: number; // TODO: 이거 필수 요소 아닌 채로도 가능한지 확인
+};
+
+export type CreateExternalResultDto = {
+  externalId: number;
+  createdAt: string;
+};
+
+export type ResponseCreateExternalDetatilDto = CommonResponse<CreateExternalResultDto>;
+
+// 외부 이슈 조회
+export type ViewExternalDetailDto = {
+  id: number;
+  name: string;
+  title: string;
+  content: string;
+  priority: string;
+  state: string;
+  startDate?: string | null; // TODO: 빼야 할 듯
+  endDate?: string | null; // TODO: 빼야 할 듯
+  goalId?: Pick<Goal, 'id'>; // TODO: 데이터 구조 통일해달라고 하기
+  goalTitle?: Pick<Goal, 'title'>; // TODO: 데이터 구조 통일해달라고 하기
+  extServiceType: string;
+  managers: Manager;
+  deadlines: Deadline; // TODO: 이름 통일해달라고 하기
+  comments: ResponseCommentListDto;
+};
+
+export type ResponseViewIssueDetailDto = CommonResponse<ViewExternalDetailDto>;
+
+// 외부 이슈 수정
+export type UpdateExternalDetailDto = {
+  // 변경사항이 없는 속성은 Null값 가능, 키 생략(undef)도 가능
+  title?: string | null;
+  content?: string | null;
+  state?: string | null;
+  priority?: string | null;
+  managersId?: number[] | null;
+  deadline?: Deadline | null;
+  goalId?: number | null;
+};
+
+export type UpdateExternalResultDto = {
+  issueId: number;
+  updatedAt: string; // LocalDateTime
+};
+
+export type ResponseUpdateIssueDetailDto = CommonResponse<UpdateExternalResultDto>;
