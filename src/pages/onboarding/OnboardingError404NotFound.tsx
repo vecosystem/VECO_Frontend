876f24562310ec93432830b5fb0e5cd4fd1f08b3
YuminPark <p353700@gmail.com>
2025-08-25 17:37:50 +0900

    #207 [DEL] onboarding 관련 페이지들 주석 제거

--- src/pages/onboarding/OnboardingError404NotFound.tsx
+++ src/pages/onboarding/OnboardingError404NotFound.tsx
@@ -1,5 +1,3 @@
-// src/pages/onboarding/OnboardingError404NotFound.tsx
-
 import { useNavigate } from 'react-router-dom';
 import PrimaryButton from '../../components/Onboarding/PrimaryButton';
 
@@ -10,7 +8,6 @@ const OnboardingError404NotFound = () => {
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-onboard">
       <div className="h-[42rem] flex flex-col justify-end">
         <div className="flex flex-col items-center gap-[10.8rem]">
-          {/* 에러 문구 */}
           <div className="flex flex-col gap-[3.2rem] text-center">
             <h2 className="font-bigtitle-b text-gray-600">404</h2>
             <h3 className="font-title-sub-r text-gray-600">
@@ -18,7 +15,6 @@ const OnboardingError404NotFound = () => {
               <br /> 요청하신 페이지가 사라졌거나, 잘못된 경로로 이용하셨어요!
             </h3>
           </div>
-          {/* 돌아가기 버튼 */}
           <PrimaryButton text="돌아가기" onClick={() => navigate(-1)} />
         </div>
       </div>
--- src/pages/onboarding/OnboardingSSOLogin.tsx
+++ src/pages/onboarding/OnboardingSSOLogin.tsx
@@ -1,24 +1,18 @@
-// src/pages/onboarding/OnboardingSSOLogin.tsx
-
 import vecologo from '../../assets/logos/vecologo.svg';
 import SocialLoginButton from '../../components/Onboarding/SocialLoginButton';
 
 const OnboardingSSOLogin = () => {
   return (
     <div className="flex flex-col items-center gap-[3.2rem]">
-      {/* 백호 로고 & 백호 텍스트*/}
       <div className="flex flex-col items-center gap-[1.6rem]">
         <img src={vecologo} alt="Veco" className="w-[12.2rem] h-[11.1726rem]" />
         <h2 className="font-title-b text-primary-blue">Veco</h2>
       </div>
-      {/* "나의 작업실을 생성해봐요" 문구 */}
       <h1 className="font-body-r text-gray-600">나의 작업실을 생성해봐요</h1>
-      {/* 소셜 로그인 버튼 영역 */}
       <div className="flex flex-col items-center gap-[1.6rem]">
         <SocialLoginButton provider="google" />
         <SocialLoginButton provider="kakao" />
       </div>
-      {/* 하단 이용약관 + 밑줄 */}
       <div
         className="flex flex-col cursor-pointer"
         onClick={() =>
